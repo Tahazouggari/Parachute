3c849a1b95951a85fb74c5a74196f810019c8494
zouggari taha <zouggaritaha6@gmail.com>
2025-03-22 03:11:43 +0100

    responsive binary view

--- src/view/BinaryWidget.cpp
+++ src/view/BinaryWidget.cpp
@@ -15,33 +15,30 @@ void BinaryWidget::setBackgroundColor(const QColor &color) {
 void BinaryWidget::paintEvent(QPaintEvent *) {
     QPainter painter(this);
 
-    // Fill the entire widget with the background color
     painter.fillRect(rect(), backgroundColor);
 
     QBrush painterBrush(painterColor);
 
-    // Calculate the size of the grid dynamically
-    int cellWidth = width() / numberLetterDraw; // Width of each cell
-    int cellHeight = height() / bitByLetter;    // Height of each cell
-    int cellSize = std::min(cellWidth, cellHeight); // Use the smaller dimension for square cells
-    int circleSize = cellSize * 0.8; // Circle size is 80% of the cell size
+    int cellWidth = width() / numberLetterDraw; 
+    int cellHeight = height() / bitByLetter;    
+    int cellSize = std::min(cellWidth, cellHeight); 
+    int circleSize = cellSize * 0.8; 
 
-    // Calculate the starting position to center the grid
+
     int gridWidth = numberLetterDraw * cellSize;
     int gridHeight = bitByLetter * cellSize;
     int startX = (width() - gridWidth) / 2;
     int startY = (height() - gridHeight) / 2;
 
-    // Draw the binary grid
     for (int i = 0; i < numberLetterDraw; ++i) {
         for (int j = 0; j < bitByLetter; ++j) {
             int index = i * bitByLetter + j;
             if (index < static_cast<int>(bitSet.size()) && bitSet.at(index)) {
-                painter.setBrush(painterBrush); // Red for 1
+                painter.setBrush(painterBrush); 
             } else {
-                painter.setBrush(backgroundColor); // Use background color for 0
+                painter.setBrush(backgroundColor); 
             }
-            // Draw circle centered in the cell
+
             int x = startX + i * cellSize + (cellSize - circleSize) / 2;
             int y = startY + j * cellSize + (cellSize - circleSize) / 2;
             painter.drawEllipse(x, y, circleSize, circleSize);
@@ -51,5 +48,5 @@ void BinaryWidget::paintEvent(QPaintEvent *) {
 
 void BinaryWidget::updateBitSet(const std::vector<bool> &b) {
     bitSet = b;
-    update(); // Trigger repaint
+    update();
 }
--- src/view/MainWindow.cpp
+++ src/view/MainWindow.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 #include <QFileDialog>
-#include <QDebug> // Add this header for qDebug()
+#include <QDebug>
 #include "../model/MessageEncoder.h"
 #include "../view/ParachuteView.h"
 #include "../view/BinaryWidget.h"
@@ -10,29 +10,22 @@
 #include <QFile>
 #include <QTextStream>
 #include <QMessageBox>
-#include <QPainter> // Include QPainter to fix the incomplete type error
+#include <QPainter>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow), languageManager(new LanguageManager(this)) {
-    ui->setupUi(this); // Set up the UI
+    ui->setupUi(this);
 
-    // Initialize parachuteView
     parachuteView = new ParachuteView(this);
-
-    // Add parachuteView to the parachuteViewTab
     QVBoxLayout *parachuteLayout = new QVBoxLayout(ui->parachuteViewTab);
     parachuteLayout->addWidget(parachuteView);
 
-    // Initialize binaryWidget
     binaryWidget = new BinaryWidget(this);
-
-    // Add binaryWidget to the binaryViewLayout
     QVBoxLayout *binaryLayout = qobject_cast<QVBoxLayout *>(ui->binaryViewTab->layout());
     if (binaryLayout) {
         binaryLayout->addWidget(binaryWidget);
     }
 
-    // Connect signals and slots
     connect(ui->messageInput, &QLineEdit::textChanged, this, &MainWindow::onMessageChanged);
     connect(ui->colorButton, &QPushButton::clicked, this, &MainWindow::onBackgroundColorChanged);
     connect(ui->exportButton, &QPushButton::clicked, this, &MainWindow::onSaveParachute);
@@ -40,7 +33,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->openAction, &QAction::triggered, this, &MainWindow::onOpenFile);
     connect(ui->exitAction, &QAction::triggered, this, &MainWindow::onExit);
 
-    // Connect sliders to update parachute view live
     connect(ui->sliderSectors, &QSlider::valueChanged, ui->spinSectors, &QSpinBox::setValue);
     connect(ui->spinSectors, QOverload<int>::of(&QSpinBox::valueChanged), ui->sliderSectors, &QSlider::setValue);
     connect(ui->sliderSectors, &QSlider::valueChanged, this, &MainWindow::onSectorsOrTracksChanged);
@@ -49,7 +41,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->spinTracks, QOverload<int>::of(&QSpinBox::valueChanged), ui->sliderTracks, &QSlider::setValue);
     connect(ui->sliderTracks, &QSlider::valueChanged, this, &MainWindow::onSectorsOrTracksChanged);
 
-    // Initialize sliders and spin boxes
     ui->sliderSectors->setRange(7, 28);
     ui->spinSectors->setRange(7, 28);
     ui->sliderTracks->setRange(3, 10);
@@ -60,12 +51,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->sliderTracks->setValue(3);
     ui->spinTracks->setValue(3);
 
-    // Connect language menu actions
     connect(ui->actionEnglish, &QAction::triggered, this, &MainWindow::onLanguageEnglish);
     connect(ui->actionfrensh, &QAction::triggered, this, &MainWindow::onLanguageFrench);
     connect(ui->actionArabic, &QAction::triggered, this, &MainWindow::onLanguageArabic);
 
-    // Connect language manager signal
     connect(languageManager, &LanguageManager::languageChanged, this, &MainWindow::retranslateUi);
 }
 
@@ -75,25 +64,17 @@ MainWindow::~MainWindow() {
 
 void MainWindow::onMessageChanged() {
     QString message = ui->messageInput->text();
-
-    // Encode the message into binary
     std::vector<int> encodedMessage = MessageEncoder::encodeMessage(message);
-
-    // Convert to bool vector for BinaryWidget
     std::vector<bool> binaryBits(encodedMessage.begin(), encodedMessage.end());
-
-    // Update binaryWidget
     binaryWidget->updateBitSet(binaryBits);
-
-    // Update the parachute view
     parachuteView->setParachuteData(ui->spinSectors->value(), ui->spinTracks->value(), encodedMessage);
 }
 
 void MainWindow::onBackgroundColorChanged() {
     QColor color = QColorDialog::getColor(Qt::white, this, "Choose Background Color");
     if (color.isValid()) {
-        parachuteView->setBackgroundColor(color); // Update Parachute View background color
-        binaryWidget->setBackgroundColor(color);  // Update Binary View background color
+        parachuteView->setBackgroundColor(color);
+        binaryWidget->setBackgroundColor(color);
     }
 }
 
@@ -101,24 +82,14 @@ void MainWindow::onSaveParachute() {
     QString filename = QFileDialog::getSaveFileName(this, "Save Image", "", "PNG Image (*.png)");
     if (!filename.isEmpty()) {
         if (!filename.endsWith(".png")) {
-            filename += ".png"; // Ensure the file has the correct extension
+            filename += ".png";
         }
-
-        // Get the size of the Parachute View
         QSize parachuteSize = parachuteView->size();
-
-        // Create a QPixmap to hold the Parachute View
         QPixmap parachutePixmap(parachuteSize);
-        parachutePixmap.fill(Qt::white); // Fill with a white background
-
+        parachutePixmap.fill(Qt::white);
         QPainter painter(&parachutePixmap);
-
-        // Render the Parachute View
         parachuteView->render(&painter);
-
         painter.end();
-
-        // Save the image
         if (parachutePixmap.save(filename)) {
             QMessageBox::information(this, "Export Image", "Parachute image saved successfully.");
         } else {
@@ -131,24 +102,18 @@ void MainWindow::onSaveFile() {
     QString filename = QFileDialog::getSaveFileName(this, "Save File", "", "Parachute Files (*.ep)");
     if (!filename.isEmpty()) {
         if (!filename.endsWith(".ep")) {
-            filename += ".ep"; // Ensure the file has the correct extension
+            filename += ".ep";
         }
-
         QFile file(filename);
         if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
             QTextStream out(&file);
-
-            // Save the message
             QString message = ui->messageInput->text();
             out << "[Message]\n" << message << "\n";
-
-            // Save the parachute parameters
             int sectors = ui->spinSectors->value();
             int tracks = ui->spinTracks->value();
             out << "[Parameters]\n";
             out << "Sectors=" << sectors << "\n";
             out << "Tracks=" << tracks << "\n";
-
             file.close();
             QMessageBox::information(this, "Save File", "File saved successfully.");
         } else {
@@ -163,17 +128,14 @@ void MainWindow::onOpenFile() {
         QFile file(filename);
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
             QTextStream in(&file);
-
             QString line;
             QString message;
             int sectors = 0;
             int tracks = 0;
-
             while (!in.atEnd()) {
-                line = in.readLine().trimmed(); // Trim whitespace
-
+                line = in.readLine().trimmed();
                 if (line == "[Message]") {
-                    message = in.readLine().trimmed(); // Read the next line as the message
+                    message = in.readLine().trimmed();
                 } else if (line == "[Parameters]") {
                     while (!in.atEnd()) {
                         line = in.readLine().trimmed();
@@ -185,15 +147,10 @@ void MainWindow::onOpenFile() {
                     }
                 }
             }
-
-            // Update the UI with the loaded data
             ui->messageInput->setText(message);
             ui->spinSectors->setValue(sectors);
             ui->spinTracks->setValue(tracks);
-
-            // Trigger updates for the parachute view
             onSectorsOrTracksChanged();
-
             file.close();
             QMessageBox::information(this, "Open File", "File loaded successfully.");
         } else {
@@ -207,13 +164,8 @@ void MainWindow::onExit() {
 }
 
 void MainWindow::onSectorsOrTracksChanged() {
-    // Get the current message
     QString message = ui->messageInput->text();
-
-    // Encode the message into binary
     std::vector<int> encodedMessage = MessageEncoder::encodeMessage(message);
-
-    // Update the parachute view with the current sectors, tracks, and encoded message
     parachuteView->setParachuteData(ui->spinSectors->value(), ui->spinTracks->value(), encodedMessage);
 }
 
@@ -230,6 +182,6 @@ void MainWindow::onLanguageArabic() {
 }
 
 void MainWindow::retranslateUi() {
-    ui->retranslateUi(this); // Retranslate the UI
+    ui->retranslateUi(this);
     qDebug() << "UI retranslated to language:" << languageManager->getCurrentLanguage();
 }
